d2f18d2b3901af82d24de89838b6a6e9b7b64d86
Keaton Freed <keatonafreed2@icloud.com>
2024-04-26 13:26:26 -0700

    finished nav bar v1 and fixed some basics

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import "components/Header.css"
+import InstagramIcon from "assets/Instagram.svg"
+import TikTokIcon from "assets/TikTok.svg"
+import YoutubeIcon from "assets/Youtube.svg"
 
 function Header() {
     return (
         <div className='Header'>
-            <a href="https://www.instagram.com/mcgrails3dprints" target='_blank' rel="noreferrer">Instagram</a>
-            <a href="https://www.tiktok.com/@mcgrail_" target='_blank' rel="noreferrer">TikTok</a>
-            <a href="http://www.youtube.com/@McGrails3DPrints" target='_blank' rel="noreferrer">Youtube</a>
+            <div className="HeaderLeft">
+                <h1>McGrails 3D Prints</h1>
+            </div>
+            <div className="HeaderRight">
+                <a href="https://www.instagram.com/mcgrails3dprints" target='_blank' rel="noreferrer"><img src={InstagramIcon} alt="Instagram" /></a>
+                <a href="https://www.tiktok.com/@mcgrail_" target='_blank' rel="noreferrer"><img src={TikTokIcon} alt="TikTok" /></a>
+                <a href="http://www.youtube.com/@McGrails3DPrints" target='_blank' rel="noreferrer"><img src={YoutubeIcon} alt="Youtube" /></a>
+            </div>
         </div>
     )
 }
