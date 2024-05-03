4771cc4f92dd5dc8552932e447c08a24c6eac5bd
Keaton Freed <keaton@mfreed.com>
2024-05-03 09:56:47 -0700

    test functions

--- api.js
+++ api.js
@@ -1,8 +1,12 @@
-fetch("https://openapi.etsy.com/v3/shops/1626500092/listings/active", {
+const env = require("dotenv").config()
+const fs = require("fs")
+
+fetch("https://openapi.etsy.com/v3/application/shops/46422638/listings/active", {
     headers: {
-        "x-api-key": ""
+        "x-api-key": process.env.ETSY_API_KEY
     }
-}).then(resp => resp.json()).then((resp) => {
+}).then(resp => resp.text()).then((resp) => {
     console.log(resp)
+    fs.writeFileSync("listings.json", resp)
 })
-console.log("sent")
+console.log("sent", process.env.ETSY_API_KEY)
