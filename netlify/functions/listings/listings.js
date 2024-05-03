a97adf5394829cd29d5cfcd67a559838a2ea4dca
Keaton Freed <keaton@mfreed.com>
2024-05-03 10:32:44 -0700

    listing function with fetch

--- netlify/functions/listings/listings.js
+++ netlify/functions/listings/listings.js
@@ -1,19 +1,49 @@
 // Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
+
+function sendSuccess(message) {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ output: message }),
+  }
+}
+function sendError(message) {
+  return {
+    statusCode: 500,
+    body: JSON.stringify({ error: message }),
+  }
+}
+
+
 const handler = async (event) => {
   try {
-    const subject = event.queryStringParameters.name || 'World'
     const apiKey = process.env.ETSY_API_KEY
-    console.log("KEY", apiKey.slice(1, 6))
-
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ message: `Hello ${subject}` }),
-      // // more keys you can return:
-      // headers: { "headerName": "headerValue", ... },
-      // isBase64Encoded: true,
+    if (!apiKey) {
+      console.log("API Key not loaded properly:", apiKey)
+      return sendError("API Error")
     }
+
+    fetch("https://openapi.etsy.com/v3/application/shops/46422638/listings/active", {
+      headers: {
+        "x-api-key": apiKey
+      }
+    }).then(resp => resp.json()).then((resp) => {
+      console.log("Got Etsy listings API response.")
+
+      let results = resp.results
+      let filtered = results.map(({ title, description, id, url, featured_rank, price }) => {
+        let priceNum = price.amount / price.divisor
+        return {
+          id, title, description, url, rank: featured_rank, price: priceNum
+        }
+      })
+      return sendSuccess(filtered)
+    }).catch((err) => {
+      console.log("API Error:", err)
+      return sendError("API Error")
+    })
   } catch (error) {
-    return { statusCode: 500, body: error.toString() }
+    console.log("Runtime Error:", error)
+    return sendError("Server Error")
   }
 }
 
