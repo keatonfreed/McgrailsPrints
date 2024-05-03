b1deb3fb83398b0dfb3cfaf6f819cf3ae03d83de
Keaton Freed <keaton@mfreed.com>
2024-05-03 10:07:01 -0700

    test functions 2

---
+++ netlify/functions/listings/listings.js
@@ -0,0 +1,20 @@
+// Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
+const handler = async (event) => {
+  try {
+    const subject = event.queryStringParameters.name || 'World'
+    const apiKey = Netlify.env.get("ETSY_API_KEY")
+    console.log("KEY", apiKey.slice(1, 6))
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: `Hello ${subject}` }),
+      // // more keys you can return:
+      // headers: { "headerName": "headerValue", ... },
+      // isBase64Encoded: true,
+    }
+  } catch (error) {
+    return { statusCode: 500, body: error.toString() }
+  }
+}
+
+module.exports = { handler }
