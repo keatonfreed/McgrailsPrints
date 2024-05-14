9c5b423e676e2a0c3d5eca16c5ec4ad3afd5e609
Keaton Freed <keaton@mfreed.com>
2024-05-14 16:51:55 -0700

    almost done with listing function

--- netlify/functions/listings/listings.js
+++ netlify/functions/listings/listings.js
@@ -29,24 +29,24 @@ function sendError(message) {
 
 
 const handler = async (event) => {
-  const { error: delError } = await supabase
-    .from('Listings')
-    .delete()
-  if (delError) return console.log("Delete Error:", delError)
+  // const { error: delError } = await supabase
+  //   .from('Listings')
+  //   .delete()
+  // if (delError) return console.log("Delete Error:", delError)
 
-  const { data: added, insError } = await supabase
-    .from('Listings')
-    .insert([
-      { data: 'Inset from API DATA LALA' },
-    ])
-  if (insError) return console.log("Delete Error:", delError)
+  // const { data: added, insError } = await supabase
+  //   .from('Listings')
+  //   .insert([
+  //     { data: 'Inset from API DATA LALA' },
+  //   ])
+  // if (insError) return console.log("Delete Error:", delError)
 
 
-  let { data: listings, error } = await supabase
-    .from('Listings')
-    .select('*')
-  console.log(listings, error)
-  return sendSuccess([added, err, listings, error])
+  // let { data: listings, error } = await supabase
+  //   .from('Listings')
+  //   .select('*')
+  // console.log(listings, error)
+  // return sendSuccess([added, err, listings, error])
 
   try {
     const apiKey = process.env.ETSY_API_KEY
@@ -55,22 +55,55 @@ const handler = async (event) => {
       return sendError("API Error")
     }
 
-    console.log("Function init, running...")
-    let { data } = await axios.get("https://openapi.etsy.com/v3/application/shops/46422638/listings/active", {
+    console.log("Function init, fetching listings...")
+    let { data: etsyData } = await axios.get("https://openapi.etsy.com/v3/application/shops/46422638/listings/active", {
       headers: {
         "x-api-key": apiKey
       },
       method: 'GET'
     })
 
-    let results = data.results
-    console.log("Got Etsy listings API response:", results.length)
+    let results = etsyData.results
+    let listingIds = []
     let filtered = results.map(({ title, description, id, url, featured_rank, price }) => {
       let priceNum = price.amount / price.divisor
+      listingIds.push(id)
       return {
         id, title, description, url, rank: featured_rank, price: priceNum
       }
     })
+    console.log("Got Etsy listings API response:", filtered.length)
+
+    const { data: imgSelectData, error: imgSelectError } = await supabase.from("Listings")
+      .select("listing_id,data")
+      .in("listing_id", listingIds)
+    if (imgSelectError) return console.log("Select Error:", imgSelectError)
+    console.log("selected all imgs from id's", imgSelectData)
+    let listingsNotFound = [...listingIds]
+    imgSelectData.forEach(({ listing_id, data }) => {
+      delete listingsNotFound[listingsNotFound.findIndex(listing_id)]
+      // filtered.find()
+    })
+    console.log("listing images not cached:", listingsNotFound)
+    for (let i = 0; i < listingsNotFound.length; i++) {
+      let { data: etsyImageData } = await axios.get(`https://openapi.etsy.com/v3/application/listings/${listingsNotFound[i]}/images`, {
+        headers: {
+          "x-api-key": apiKey
+        },
+        method: 'GET'
+      })
+      let etsyImageResults = etsyImageData.results
+      console.log(etsyImageResults, etsyImageResults.url_fullxfull)
+
+      const { data: added, insError } = await supabase
+        .from('Listings')
+        .insert([
+          { listing_id: listingsNotFound[i], data: etsyImageResults },
+        ])
+      if (insError) return console.log("Insert Error:", insError)
+      await new Promise((res) => { setTimeout(res, 500) })
+    }
+
     console.log("Function Success, sending:", filtered.length)
     return sendSuccess(filtered)
   } catch (error) {
