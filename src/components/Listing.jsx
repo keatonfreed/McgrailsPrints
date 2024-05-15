a74975322cc954b148d6693bebf70b1756b38273
Keaton Freed <keatonafreed2@icloud.com>
2024-05-14 21:50:53 -0700

    fix img alt text

--- src/components/Listing.jsx
+++ src/components/Listing.jsx
@@ -7,7 +7,7 @@ function Listing({ listing }) {
     const decodedTitle = listing.title ? he.decode(listing.title) : "Loading...";
 
     return (
-        <a href={listing.url || ""} target="_blank" rel="noreferrer" className='Listing'>
+        <a href={listing.url || "#0"} target="_blank" rel="noreferrer" className='Listing'>
             <img src={listing?.image?.[0]?.["url_fullxfull"] || "https://blank"} alt="Listing Display" srcset="" />
             <div className='ListingDetails'>
                 <h1 className='ListingTitle'>{decodedTitle}</h1>
