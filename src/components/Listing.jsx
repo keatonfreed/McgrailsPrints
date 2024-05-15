d09246ff5d4937a32fe117697ed7803b00ed6df2
Keaton Freed <keatonafreed2@icloud.com>
2024-05-14 21:24:53 -0700

    add link to real etsy listing

--- src/components/Listing.jsx
+++ src/components/Listing.jsx
@@ -4,17 +4,17 @@ import he from 'he';
 
 
 function Listing({ listing }) {
-    const decodedTitle = he.decode(listing.title);
+    const decodedTitle = listing.title ? he.decode(listing.title) : "Loading...";
 
     return (
-        <div className='Listing'>
+        <a href={listing.url || ""} target="_blank" rel="noreferrer" className='Listing'>
             <img src={listing?.image?.[0]?.["url_fullxfull"] || "https://blank"} alt="Listing Display" srcset="" />
             <div className='ListingDetails'>
                 <h1 className='ListingTitle'>{decodedTitle}</h1>
                 <h1 className='ListingPrice'>${listing.price}</h1>
             </div>
             <div className="ListingBorder"></div>
-        </div>
+        </a>
     )
 }
 
